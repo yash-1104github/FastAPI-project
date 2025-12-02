cfcfdb4a2f03ce6c9c82ce7aaa9a936208952668
yash-1104github <gyah1104@gmail.com>
2025-12-02 18:24:27 +0530

    chore: Updates frontend_url

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,14 @@
 import logging
+from dotenv import load_dotenv
+import os
 from fastapi import FastAPI
+
 from .routers import rooms, ws
 from .database import Base, engine
 from . import models
+from fastapi.middleware.cors import CORSMiddleware
+
+load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("app")
@@ -11,9 +17,22 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+
+origins = [
+    FRONTEND_URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(rooms.router)
 app.include_router(ws.router)
-
 
 @app.get("/")
 def health():
